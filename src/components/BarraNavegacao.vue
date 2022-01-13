74987331644e4541cee358e1eae5d7bc1f325a00
Takashi <ygortn14@gmail.com>
2022-01-12 21:29:58 -0400

    :tada: Capitulo 3: Refatorando e evoluindo o código Aprendi sobre: - Decomposição de componentes em componentes menores - Implementação e configuração do vuex - As responsabilidades diferentes de mutations e actions

---
+++ src/components/BarraNavegacao.vue
@@ -0,0 +1,47 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-bytebank">
+      <a class="navbar-brand" href="#">ByteBank</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <BarraNavegacaoQuandoLogado v-if="usuarioestaLogado" />
+            <BarraNavegacaoQuandoDeslogado v-else />
+      </div>
+    </nav>
+</template>
+
+<script>
+import BarraNavegacaoQuandoLogado from './BarraNavegacaoQuandoLogado.vue'
+import BarraNavegacaoQuandoDeslogado from './BarraNavegacaoQuandoDeslogado.vue'
+
+export default {
+    components: {
+        BarraNavegacaoQuandoLogado,
+        BarraNavegacaoQuandoDeslogado
+    },
+    methods: {
+        efetuarLogout() {
+            localStorage.removeItem('token');
+            this.$router.push({name: 'login'}); 
+        }
+    },
+    computed:{
+        usuarioestaLogado() {
+            return Boolean(this.$store.state.token);
+        }
+    }
+}
+</script>
+
+<style>
+.navbar {
+  background: #27ae60;
+}
+.navbar-bytebank a {
+  color: #fff;
+}
+.navbar-bytebank a:hover {
+  color: #000;
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -30,13 +30,10 @@ export default {
     },
     methods:{
         efetuarLogin(){
-            this.$http.post('http://localhost:8000/auth/login', this.usuario)
-                .then(resposta => {
-                        console.log(resposta)
-                        localStorage.setItem('token', resposta.data.access_token);
-                        this.$router.push({ name: 'gerentes' });
-                    })
-                .catch(erro => console.log(erro));
+            this.$store.dispatch('efetuarLogin', this.usuario)
+            .then(() => {
+                this.$router.push({name: 'gerentes'})
+            })
         }
     }
 }
